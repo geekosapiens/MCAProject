980d1aadfb2b2ae2e694d56dcf802822605b246b
“geekosapiens” <anasbaig10@gmail.com>
2022-09-11 07:14:53 +0000

    new

--- DockerComposeInstall.sh
+++ DockerComposeInstall.sh
@@ -3,4 +3,4 @@ sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
 
-docker-compose docker-compose.yml build
+docker-compose build -d
