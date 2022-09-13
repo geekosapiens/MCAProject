71a988e712d1943807b95194b9a6aab3ca565f92
“geekosapiens” <anasbaig10@gmail.com>
2022-09-13 03:49:17 +0000

    final

--- DBSetup.sh
+++ DBSetup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-docker exec -it project-db-1 mysql -uroot -proot -e "CREATE DATABASE db;"
-docker exec -it project-db-1 mysql -uroot -proot -e "USE db;"
-docker exec -it project-db-1 mysql -uroot -proot db < ./db/formdata.sql
+read -p "Enter database Container name:  " NAME
+
+docker exec -it $NAME mysql -uroot -proot -e "CREATE DATABASE db;"
+docker exec -it $NAME mysql -uroot -proot -e "USE db;"
+docker exec -i $NAME mysql -uroot -proot db < ./db/formdata.sql
+
